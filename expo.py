ae3def94bc60be5cf3ab3cc2810ec1a8ec05f454
Nikhil <nprathap@cisco.com>
2021-04-10 22:29:32 -0700

    change to expo

--- expo.py
+++ expo.py
@@ -5,32 +5,32 @@ if __name__ == "__main__":
       age = float(input("Please enter child's age in years: "))
 
       if gender=="boy":
-        if 7<=age<=10:
+        if 7<age<10:
           print("can participate in ",final_out[1])
           print("\n-------------------------------------------------------\n")
-        elif 11<=age<=15:
+        elif 11<age<15:
           print("can participate in ",final_out[3])
           print("\n-------------------------------------------------------\n")
-        elif age<=6:
+        elif age<6:
           print("can participate in ",final_out[5])
           print("\n-------------------------------------------------------\n")
-        elif age>=20:
+        elif age>20:
           print("can participate in ",final_out[6])
           print("\n-------------------------------------------------------\n")
         else:
           print("invalid age input")
           print("\n-------------------------------------------------------\n")
       elif gender=="girl":
-        if 7<=age<=10:
+        if 7<age<10:
           print("can participate in ",final_out[2])
           print("\n-------------------------------------------------------\n")
-        elif 11<=age<=15:
+        elif 11<age<15:
           print("can participate in ",final_out[4])
           print("\n-------------------------------------------------------\n")
-        elif age<=6:
+        elif age<6:
           print("can participate in ",final_out[5])
           print("\n-------------------------------------------------------\n")
-        elif age>=20:
+        elif age>20:
           print("can participate in ",final_out[6])
           print("\n-------------------------------------------------------\n")
         else:
